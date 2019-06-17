63a4f10f3d31dfb6e5895bd1340d6a4cf553dd1c
Evelyn <evelynlivant@gmail.com>
2019-06-17 15:23:07 -0400

    sql dump for entire db

---
+++ SQL Dump/dob_medication_related_problem_categories.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dob
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `medication_related_problem_categories`
+--
+
+DROP TABLE IF EXISTS `medication_related_problem_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `medication_related_problem_categories` (
+  `Medication_Related_Problem_Code` varchar(255) NOT NULL,
+  `Category` varchar(255) DEFAULT NULL,
+  `Action_Plan` text,
+  PRIMARY KEY (`Medication_Related_Problem_Code`),
+  CONSTRAINT `medication_related_problem_categories_ibfk_1` FOREIGN KEY (`Medication_Related_Problem_Code`) REFERENCES `medication_related_problem` (`Medication_Related_Problem_Code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medication_related_problem_categories`
+--
+
+LOCK TABLES `medication_related_problem_categories` WRITE;
+/*!40000 ALTER TABLE `medication_related_problem_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medication_related_problem_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-06-17 15:21:31
---
+++ SQL Dump/dob_medication_review_session.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dob
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `medication_review_session`
+--
+
+DROP TABLE IF EXISTS `medication_review_session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `medication_review_session` (
+  `Patient_ID` int(11) NOT NULL,
+  `Drug_ID` int(11) DEFAULT NULL,
+  `Session_Date` date DEFAULT NULL,
+  `Instructions` varchar(255) DEFAULT NULL,
+  `Indication` varchar(255) DEFAULT NULL,
+  `How_Long_Taking` varchar(255) DEFAULT NULL,
+  `Adherence` tinyint(1) DEFAULT NULL,
+  `Side_Effects` tinyint(1) DEFAULT NULL,
+  `Administration_Technique` tinyint(1) DEFAULT NULL,
+  `Formulary_Friendly` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`Patient_ID`),
+  KEY `CompositeMedReview` (`Patient_ID`,`Drug_ID`,`Session_Date`),
+  KEY `Drug_ID` (`Drug_ID`),
+  CONSTRAINT `medication_review_session_ibfk_1` FOREIGN KEY (`Patient_ID`) REFERENCES `patients` (`Patient_ID`),
+  CONSTRAINT `medication_review_session_ibfk_2` FOREIGN KEY (`Drug_ID`) REFERENCES `drug` (`Drug_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medication_review_session`
+--
+
+LOCK TABLES `medication_review_session` WRITE;
+/*!40000 ALTER TABLE `medication_review_session` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medication_review_session` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-06-17 15:21:33
